d95361202231abcab626425a76e0ce494e15c3e3
ryanw <ryan02baum@gmail.com>
2025-04-20 02:39:39 +0000

    adding updated main and new circleshape class

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

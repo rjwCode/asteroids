63696ee02cce7805ab6dcdb2ec5c316166b1364c
ryanw <ryan02baum@gmail.com>
2025-04-21 00:05:30 +0000

    Added asteroids, asteroid field. Updated game logic to include asteroid movement and collisions.

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    #Console Log Output
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    #Game initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    #Defining sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    #Defining class containers
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    #Defining class instances
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+    field = AsteroidField()
+
+    #Starting Game Loop
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,6 +39,11 @@ def main():
         for object in drawable:
             object.draw(screen)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(player) == True:
+                print("Game over!")
+                exit()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
